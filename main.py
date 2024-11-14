49fde9ca903abbefced8832c389f595fd9210dde
Leah641 <2875167@student.dublinisc.com>
2024-11-14 10:15:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,16 @@ if x<10000 and x>=1000:
     if not x%10==0:
     
         if x%2==0:
-        
             print("It is good because it's even.")
         
         else:
-        
             print("It is bad because it's not even.")
     
     else:
-    
         print("It is bad because it ends with 0.")
 
-else:print("It is bad because it's not 4 digits.")
+else:
+    print("It is bad because it's not 4 digits.")
 
 
 quit()
